cdf91b64638e34aa5e3a805f843a6f397ac30cf1
m1rin2007 <karotte.monoclock@gmail.com>
2026-05-28 09:21:39 +0800

    build_7 20260527 # 新增start命令，改进quit命令

--- cli/command.cpp
+++ cli/command.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include "include/game.h"
@@ -17,11 +18,52 @@ bool Game::process(std::vector<std::string> &args) {
 
     std::string main_cmd = args[0];
     if(main_cmd == "quit") {
-        return false;
+        return cmd_quit();
+    } else if(main_cmd == "start") {
+        cmd_start(args);
     } else if(main_cmd == "version") {
         print_version();
         print_author();
     }
 
     return true;
+}
+
+bool Game::cmd_quit() {
+    if(!is_playing) {
+        return false;
+    }
+
+    std::string confirm;
+    std::cout << "是否要退出对局？输入yes和回车确认。 ";
+    std::getline(std::cin, confirm);
+
+    if(confirm == "yes") {
+        is_playing = false;
+        status = GAME_STATUS::NO_PLAYING;
+    }
+    return true;
+}
+
+void Game::cmd_start(std::vector<std::string> &args) {
+    if(is_playing) {
+        std::cout << "对局已经开始了。" << std::endl;
+        return;
+    }
+
+    std::string config;
+    if(args.size() < 2) {
+        config = "2p";
+    } else {
+        config = args[1];
+    }
+
+    is_playing = true;
+    // board.init();
+    if(config == "2p") {
+        status = GAME_STATUS::PLAYING_2P;
+    } else {
+        is_playing = false;
+        std::cout << "命令格式：start [2p/1p/1plv1/1plv2/1plv3]" << std::endl;
+    }
 }
--- cli/include/game.h
+++ cli/include/game.h
@@ -4,6 +4,12 @@
 #include <vector>
 #include "../../core/include/board.h"
 
+enum class GAME_STATUS {
+    PLAYING_2P,
+    PLAYING_1P,
+    NO_PLAYING
+};
+
 class Game {
 public:
     void print_statusline();
@@ -13,6 +19,10 @@ private:
     void print_status();
     bool process(std::vector<std::string> &args);
 
+    bool cmd_quit();
+    void cmd_start(std::vector<std::string> &args);
+
     Board board;
-    bool is_playing = false;   
+    bool is_playing = false;
+    GAME_STATUS status = GAME_STATUS::NO_PLAYING;
 };
